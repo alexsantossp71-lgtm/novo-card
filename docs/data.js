8aaf5fe9cb8ee2162f29d6de85fb84326c091c67
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2025-12-26 14:29:41 -0300

    Auto-update: New cards generated

--- docs/data.js
+++ docs/data.js
@@ -1,1 +1,32 @@
-const newsData = [];
+const newsData = [
+  {
+    "id": "1_Governo_de_SP_pede_redução_imediata_do_consumo_de_",
+    "title": "Governo de SP pede 'redução imediata do consumo de água' por causa do calor e estiagem",
+    "date": "2025-12-25T20:53:41Z",
+    "url": "https://www.terra.com.br/noticias/brasil/cidades/governo-de-sp-pede-reducao-imediata-do-consumo-de-agua-por-causa-do-calor-e-estiagem,61a5c363d20055b0db6747fbb215816dwvvrqzk0.html",
+    "assets_path": "assets/1_Governo_de_SP_pede_redução_imediata_do_consumo_de_",
+    "zip_path": "assets/1_Governo_de_SP_pede_redução_imediata_do_consumo_de_/1_Governo_de_SP_pede_redução_imediata_do_consumo_de_.zip",
+    "content": {
+      "introduction": {
+        "text": "",
+        "image": "introduction.png",
+        "prompt": ""
+      },
+      "development": {
+        "text": "",
+        "image": "development.png",
+        "prompt": ""
+      },
+      "conclusion": {
+        "text": "",
+        "image": "conclusion.png",
+        "prompt": ""
+      },
+      "general_summary": {
+        "image": "general_summary.png",
+        "prompt": ""
+      },
+      "tiktok_summary": ""
+    }
+  }
+];
