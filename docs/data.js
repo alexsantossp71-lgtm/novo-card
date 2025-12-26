0ef86d089f0990c186e2c330e4aed92c08fc0545
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2025-12-26 20:08:12 -0300

    Auto-update: New cards generated

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,35 @@
 const newsData = [
   {
+    "id": "2_Hugo_Motta_defende_debate_sem_ideologia_sobre_fim_",
+    "title": "Hugo Motta defende debate sem ideologia sobre fim da escala 6x1 em 2026",
+    "date": "2025-12-26T19:46:52Z",
+    "url": "https://www.terra.com.br/noticias/brasil/politica/hugo-motta-defende-debate-sem-ideologia-sobre-fim-da-escala-6x1-em-2026,dbcd1654285494ccc1996ede8f3d801c9pvx7p36.html",
+    "assets_path": "assets/2_Hugo_Motta_defende_debate_sem_ideologia_sobre_fim_",
+    "zip_path": "assets/2_Hugo_Motta_defende_debate_sem_ideologia_sobre_fim_/2_Hugo_Motta_defende_debate_sem_ideologia_sobre_fim_.zip",
+    "content": {
+      "introduction": {
+        "text": "",
+        "image": "introduction.png",
+        "prompt": ""
+      },
+      "development": {
+        "text": "",
+        "image": "development.png",
+        "prompt": ""
+      },
+      "conclusion": {
+        "text": "",
+        "image": "conclusion.png",
+        "prompt": ""
+      },
+      "general_summary": {
+        "image": "general_summary.png",
+        "prompt": ""
+      },
+      "tiktok_summary": ""
+    }
+  },
+  {
     "id": "1_Por_que_a_esquerda_encolhe_a_cada_ano",
     "title": "Por que a esquerda encolhe a cada ano",
     "date": null,
